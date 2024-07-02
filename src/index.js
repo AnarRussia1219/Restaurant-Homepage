5789b22baf92d9b98ebd9ada148059f6e3fa2f36
AnarRussia1219 <ft2j5fqf@duck.com>
2024-07-02 14:31:03 +0100

    create the hero section texts and the animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./root.css";
+
+let companyData = {
+    customers: "16472"
+};
+
+const elementHeroSectionCustomersNumberAnimated = document.getElementById("hero-section-customers-number-animated");
+// animation, do after 500 ms
+setTimeout(() => {
+    // start with the initial value of the element set in html
+    const b = Number(elementHeroSectionCustomersNumberAnimated.textContent);
+    // console.log(b);
+    for (let i = b; i < companyData.customers; i += 6) {
+        setTimeout(() => {
+            elementHeroSectionCustomersNumberAnimated.textContent = i;
+        }, 1);
+    };
+}, 500);
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,19 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
-            
+            colors: {
+                "accent-primary": "#ffffff",
+                "accent-primary-contrasting": "#000000",
+                "accent-secondary": "#ffd324",
+                "accent-secondary-contrasting": "#000000",
+            },
         },
+        borderRadius: {
+            "button": "8px",
+        },
+        transitionDuration: {
+            "default-1": "0.2s",
+    }
     },
     plugins: [],
 };
